2c7f25f5a15ed20690d4b4963a1285bac3216b74
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2023-11-27 10:02:07 +0000

    Update 'block-chain.js'

--- block-chain.js
+++ block-chain.js
@@ -22,10 +22,10 @@ function blockChain(data, prev = { index: 0, hash: '0' }) {
 
 //  sol 2 to fix
 
-// const blockChain = (data, prev = { index: 0, hash: '0' }) => ({
-//   index: prev.index + 1,
-//   hash: hashCode(prev.index + prev.hash + JSON.stringify(data)),
-//   data,
-//   prev,
-//   chain: newData => blockChain(newData, { index: prev.index + 1, hash: hashCode(prev.index + prev.hash + JSON.stringify(data)) }),
-// });
+const blockChain_0 = (data, prev = { index: 0, hash: '0' }) => ({
+  index: prev.index + 1,
+  hash: hashCode(prev.index + prev.hash + JSON.stringify(data)),
+  data,
+  prev,
+  chain: newData => blockChain(newData, { index: prev.index + 1, hash: hashCode(prev.index + prev.hash + JSON.stringify(data)) }),
+});
