f9e605e897ae06e060f9cb805232bd5deca4b897
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2023-11-27 09:48:00 +0000

    Update 'block-chain.js'

--- block-chain.js
+++ block-chain.js
@@ -1,30 +1,31 @@
-// // sol 1
+// sol 1
 
-// function blockChain(data, prev = { index: 0, hash: '0' }) {
-//   const index = prev.index + 1;
-//   const prevHash = prev.hash;
-//   const dataString = JSON.stringify(data);
-//   const hash = hashCode(index + prevHash + dataString);
+function blockChain(data, prev = { index: 0, hash: '0' }) {
+  const index = prev.index + 1;
+  const prevHash = prev.hash;
+  const dataString = JSON.stringify(data);
+  const hash = hashCode(index + prevHash + dataString);
 
-//   const newBlock = {
-//     index,
-//     hash,
-//     data,
-//     prev,
-//     chain: function (newData) {
-//       return blockChain(newData, newBlock);
-//     },
-//   };
+  const newBlock = {
+    index,
+    hash,
+    data,
+    prev,
+    chain: function (newData) {
+      return blockChain(newData, newBlock);
+    },
+  };
 
-//   return newBlock;
-// }
+  return newBlock;
+}
 
-/// sol 2 
 
-const blockChain = (data, prev = { index: 0, hash: '0' }) => ({
-  index: prev.index + 1,
-  hash: hashCode(prev.index + prev.hash + JSON.stringify(data)),
-  data,
-  prev,
-  chain: newData => blockChain(newData, { index: prev.index + 1, hash: hashCode(prev.index + prev.hash + JSON.stringify(data)) }),
-});
+//  sol 2 to fix
+
+// const blockChain = (data, prev = { index: 0, hash: '0' }) => ({
+//   index: prev.index + 1,
+//   hash: hashCode(prev.index + prev.hash + JSON.stringify(data)),
+//   data,
+//   prev,
+//   chain: newData => blockChain(newData, { index: prev.index + 1, hash: hashCode(prev.index + prev.hash + JSON.stringify(data)) }),
+// });
