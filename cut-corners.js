98a44d2609a05261306536dbc902b04c3eec32e5
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2023-11-30 16:24:30 +0000

    Update 'cut-corners.js'

--- cut-corners.js
+++ cut-corners.js
@@ -2,55 +2,74 @@ function modulo(v1, v2) {
     var namir = v1 < 0 ? -1 : 1;
     var di = Math.abs(v1);
     var d = Math.abs(v2);
-    if (di === 0) {return 0;}
-    if (d === 0 || isNaN(di) || isNaN(d)) {return NaN;}
-    if (di < d) {return (namir*di);}var elem_h1 = di;while (elem_h1 >= d) {elem_h1 = elem_h1 - d;}
-    return namir*elem_h1;
-  }
+    if (di === 0) {
+        return 0;
+    }
+    if (d === 0 || isNaN(di) || isNaN(d)) {
+        return NaN;
+    }
+    if (di < d) {
+        return (namir * di);
+    }
+    var elem_h1 = di;
+    while (elem_h1 >= d) {
+        elem_h1 = elem_h1 - d;
+    }
+    return namir * elem_h1;
+}
 
-function round(a){
+function round(a) {
     var nb = a;
-    if(nb<0){ nb = -nb}
-    if (nb>68719476735){
-        for (var i =68719476735;i<=nb;i++){
-            if(nb-i<=1){
-                dec = nb-i;
+    if (nb < 0) {
+        nb = -nb
+    }
+    if (nb > 68719476735) {
+        for (var i = 68719476735; i <= nb; i++) {
+            if (nb - i <= 1) {
+                dec = nb - i;
             }
         }
-    }else{var dec = modulo(nb,1);}
+    } else {
+        var dec = modulo(nb, 1);
+    }
     var s = 0.5;
-    if (dec >= s){nb++}
-    if (a<0){return -(nb-dec)}else{return nb-dec}
+    if (dec >= s) {
+        nb++
+    }
+    if (a < 0) {
+        return -(nb - dec)
+    } else {
+        return nb - dec
+    }
 }
 
-function ceil(a){
-    var s = round(a) >= a ? round(a) : round(a)+1 ;
+function ceil(a) {
+    var s = round(a) >= a ? round(a) : round(a) + 1;
     return s
 }
-function floor(a){
-    var s = round(a) <= a ? round(a) : round(a)-1 ;
+
+function floor(a) {
+    var s = round(a) <= a ? round(a) : round(a) - 1;
     return s
 }
-function trunc(a){
+
+function trunc(a) {
     var nb = a;
-    if(nb<0){ nb = -nb}
-    if (nb>68719476735){
-        for (var i =68719476735;i<=nb;i++){
-            if(nb-i<=1){
-                dec = nb-i;
+    if (nb < 0) {
+        nb = -nb
+    }
+    if (nb > 68719476735) {
+        for (var i = 68719476735; i <= nb; i++) {
+            if (nb - i <= 1) {
+                dec = nb - i;
             }
         }
-    }else{var dec = modulo(nb,1);}
-    if (a<0){return -(nb-dec)}else{return nb-dec}
+    } else {
+        var dec = modulo(nb, 1);
+    }
+    if (a < 0) {
+        return -(nb - dec)
+    } else {
+        return nb - dec
+    }
 }
-
-// var x =(68719476738);
-
-// console.log(round(x))
-// console.log(Math.round(x))
-// console.log(ceil(x))
-// console.log(Math.ceil(x))
-// console.log(floor(x))
-// console.log(Math.floor(x))
-// console.log(trunc(x))
-// console.log(Math.trunc(x))
