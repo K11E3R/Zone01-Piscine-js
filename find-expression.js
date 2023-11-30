a199a3b6437749f609e3a66d3a57dbbd7c3cf5c8
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2023-11-30 12:30:42 +0000

    Update 'find-expression.js'

--- find-expression.js
+++ find-expression.js
@@ -1,47 +1,20 @@
-function findExpression(a) {
-  if (typeof a === 'number') {
-    switch (a) {
-      case 8: {
-        return 8
-      }
-      case 14: {
-        return 14
-      }
-      case 60: {
-        return 60
-      }
-      case 100: {
-        return 100
-      }
-      case 280: {
-        return 280
-      }
-      case 110: {
-        return 110
-      }
-      case 144: {
-        return 144
-      }
-      case 200: {
-        return 200
-      }
-      case 104: {
-        return 104
-      }
-      case 7: {
-        return undefined
-      }
-      case 63: {
-        return undefined
-      }
-      case 23: {
-        return undefined
-      }
-      case 103: {
-        return undefined
-      }
+function findExpression(number) {
+  let result = 1;
+  let expression = "";
+
+  while (result < number) {
+    if ((result + 4) <= number) {
+      result += 4;
+      expression += "+4 ";
+    } else {
+      result = 2;
+      expression += "2 ";
     }
   }
-  aa = /[5-9]/g.test(code) && code.includes('add4') && code.includes('mul2')
-  return aa
+
+  if (result === number) {
+    return expression.trim();
+  } else {
+    return undefined;
+  }
 }
