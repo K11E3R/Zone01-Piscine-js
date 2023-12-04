addfe9cd057df4935f6de4d45c3d996a4b4c6233
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2023-12-04 20:38:24 +0000

    Update 'unicode-technical-report-35.js'

--- unicode-technical-report-35.js
+++ unicode-technical-report-35.js
@@ -1,38 +1,61 @@
-function format(givenDate, formatGiven) {
+function format(givenDate, formatGiven){
     let res = String(formatGiven);
     let date = new Date(givenDate);
 
-    const replaceToken = (token, value) => {
-        res = res.replace(new RegExp(token, 'g'), value);
-    };
-
-    replaceToken(/yyyy/g, Math.abs(date.getFullYear()).toString().padStart(4, '0'));
-    replaceToken(/y/g, Math.abs(date.getFullYear()));
-    replaceToken(/dd/g, date.getDate().toString().padStart(2, '0'));
-    replaceToken(/d/g, date.getDate().toString());
-    replaceToken(/hh/g, String(H(date)).padStart(2, '0'));
-    replaceToken(/h/g, H(date));
-    replaceToken(/mm/g, date.getMinutes().toString().padStart(2, '0'));
-    replaceToken(/m/g, date.getMinutes());
-    replaceToken(/MMMM/g, monthsLong[date.getMonth()]);
-    replaceToken(/MMM/g, months[date.getMonth()]);
-    replaceToken(/MM/g, (date.getMonth() + 1).toString().padStart(2, '0'));
-    replaceToken(/M/g, (date.getMonth() + 1).toString());
-    replaceToken(/EEEE/g, daysLong[date.getDay()]);
-    replaceToken(/E/g, days[date.getDay()]);
-
-    if (formatGiven.includes('a')) {
-        replaceToken(/a/g, a(date));
+    if (res.includes("yyyy")){
+        res = res.replace(/yyyy/g, Math.abs(date.getFullYear()).toString().padStart(4, "0"))
+    } else {
+        res = res.replace(/y/g, Math.abs(date.getFullYear()))
     }
 
-    replaceToken(/ss/g, date.getSeconds().toString().padStart(2, '0'));
-    replaceToken(/s/g, date.getSeconds());
-    replaceToken(/HH/g, date.getHours().toString().padStart(2, '0'));
-    replaceToken(/H/g, date.getHours());
-    replaceToken(/GGGG/g, gggg(date));
-    replaceToken(/G/g, g(date));
 
-    return res;
+    if (res.includes("dd")){
+        res = res.replace(/dd/g, date.getDate().toString().padStart(2, "0"))
+    }else {
+        res = res.replace(/d/g, date.getDate().toString())
+    }
+    if (res.includes("hh")){
+        res = res.replace(/hh/g, String(H(date)).padStart(2, "0"))
+    }else{
+        res = res.replace(/h/g, H(date))
+    }
+
+    if (res.includes("mm")){
+        res = res.replace(/mm/g, date.getMinutes().toString().padStart(2, "0"))
+    }else {
+        res = res.replace(/m/g, date.getMinutes())
+    }
+
+    if (res.includes("MMMM")){
+        res = res.replace(/MMMM/g, monthsLong[date.getMonth()])
+    } else if (res.includes("MMM")){
+         res = res.replace(/MMM/g, months[date.getMonth()])
+    } else if (res.includes("MM")){
+         res = res.replace(/MM/g, (date.getMonth()+1).toString().padStart(2, "0"))
+    } else {
+        res = res.replace(/M/g, (date.getMonth()+1).toString())
+    }
+
+    if (res.includes("EEEE")){
+        res = res.replace(/EEEE/g, daysLong[date.getDay()])
+    } else {
+        res = res.replace(/E/g, days[date.getDay()])
+    }
+
+    if (formatGiven.includes("a")){
+        res =  res.replace(/a/g, a(date))
+    }
+    res = res.replace(/ss/g, date.getSeconds().toString().padStart(2, "0"))
+    res = res.replace(/s/g, date.getSeconds())
+    
+    res = res.replace(/HH/g, date.getHours().toString().padStart(2, "0"))
+    res = res.replace(/H/g, date.getHours())
+
+    res = res.replace(/GGGG/g, gggg(date))
+    res = res.replace(/G/g, g(date))
+    
+
+    return res
 }
 
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
@@ -40,18 +63,33 @@ const monthsLong = ['January', 'February', 'March', 'April', 'May', 'June', 'Jul
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const daysLong = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-function H(date) {
-    return date.getHours() % 12;
+function H(date){
+    var hours = date.getHours();
+    return hours%12
 }
 
-function a(date) {
-    return date.getHours() >= 12 ? 'PM' : 'AM';
+function a(date){
+    var hours = date.getHours();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    return ampm
 }
 
-function gggg(date) {
-    return date.getFullYear() > 0 ? 'Anno Domini' : 'Before Christ';
+function gggg(date){
+    let res = ""
+    if (date.getFullYear() > 0){
+        res = "Anno Domini"
+    } else {
+        res = "Before Christ"
+    }
+    return res
 }
 
-function g(date) {
-    return date.getFullYear() > 0 ? 'AD' : 'BC';
+function g(date){
+    let res = ""
+    if (date.getFullYear() > 0){
+        res = "AD"
+    }else {
+        res = "BC"
+    }
+    return res
 }
