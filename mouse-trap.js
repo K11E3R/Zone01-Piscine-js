744373dcd00b52cff60905b13d60c257662b1307
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2023-12-07 11:37:34 +0000

    Update 'mouse-trap.js'

--- mouse-trap.js
+++ mouse-trap.js
@@ -1,51 +1,72 @@
 export function createCircle() {
     document.addEventListener('mousedown', event => {
-        const newCirc = document.createElement('div');
-        newCirc.className = 'circle';
-        newCirc.id = 'Tester';
-        const x = event.clientX - 25;
-        const y = event.clientY - 25;
-        newCirc.style.cssText = `left: ${x}px; top: ${y}px; background: white;`;
+        const newCirc = document.createElement('div')
+        newCirc.setAttribute('class', 'circle')
+        newCirc.setAttribute('id', 'Tester')
+        let x = event.clientX - 25
+        let y = event.clientY - 25
+        newCirc.setAttribute('style', 'left: ' + x.toString() + 'px; top: ' + y.toString() + 'px; background: white;')
         document.body.appendChild(newCirc);
-    });
+    })
 }
 
 export function moveCircle() {
+
+
     document.addEventListener('mousemove', event => {
-        const lastCircle = document.querySelector('div:last-child');
-        lastCircle.style.left = `${event.clientX - 25}px`;
-        lastCircle.style.top = `${event.clientY - 25}px`;
-        document.body.append(lastCircle);
-        const midBox = document.querySelector('div.box');
-        const dims = midBox.getBoundingClientRect();
+        const lastCircle = document.querySelector('div:last-child')
+        lastCircle.style.left = `${event.clientX - 25}px`
+        lastCircle.style.top = `${event.clientY - 25}px`
+        document.body.append(lastCircle)
+        let midBox = document.querySelector('div.box')
+        let dims = midBox.getBoundingClientRect()
 
-        if (lastCircle.className !== 'box') {
-            const isInsideX = (x) => x > dims.x && x < dims.right - 50;
-            const isInsideY = (y) => y > dims.top && y < dims.bottom - 50;
 
-            if (isInsideX(+lastCircle.style.left.replace('px', '')) && isInsideY(+lastCircle.style.top.replace('px', ''))) {
-                lastCircle.style.background = 'var(--purple)';
+        if (lastCircle.getAttribute('class') !== 'box') {
+            if ((+lastCircle.style.left.replace('px', '') > (dims.x)) && (+lastCircle.style.left.replace('px', '') < (dims.right - 50)) && (+lastCircle.style.top.replace('px', '') > (dims.top)) && (+lastCircle.style.top.replace('px', '') < (dims.bottom - 50))) {
+                lastCircle.style.background = 'var(--purple)'
             }
         }
 
-        const adjustPosition = (axis, bound) => {
-            if (event[axis] - 25 < bound) {
-                lastCircle.style[axis] = bound.toString() + 'px';
-            } else if (event[axis] - 25 > dims[bound] - 50) {
-                lastCircle.style[axis] = (dims[bound] - 50).toString() + 'px';
-            }
-        };
+        if (event.clientX - 25 < (dims.x) && lastCircle.style.background === 'var(--purple)') {
+            console.log(lastCircle.style.left)
+            lastCircle.style.left = (dims.x).toString() + 'px'
 
-        if (lastCircle.style.background === 'var(--purple)') {
-            adjustPosition('left', 'x');
-            adjustPosition('top', 'top');
-            adjustPosition('top', 'bottom');
+            if (event.clientY - 25 < (dims.top)) {
+                lastCircle.style.top = (dims.y).toString() + 'px'
+            }
+            console.log(event.clientY - 25)
+            console.log(dims.bottom)
+
+            if (event.clientY - 25 > (dims.bottom - 50)) {
+                lastCircle.style.top = (dims.bottom - 50).toString() + 'px'
+            }
+        } else if (event.clientX - 25 > (dims.right - 50) && lastCircle.style.background === 'var(--purple)') {
+            lastCircle.style.left = (dims.right - 50).toString() + 'px'
+
+            if (event.clientY - 25 < (dims.top)) {
+                lastCircle.style.top = (dims.y).toString() + 'px'
+            }
+            console.log(event.clientY - 25)
+            console.log(dims.bottom)
+
+            if (event.clientY - 25 > (dims.bottom - 50)) {
+                lastCircle.style.top = (dims.bottom - 50).toString() + 'px'
+            }
+        } else if ((event.clientY - 25 > (dims.bottom - 50)) && lastCircle.style.background === 'var(--purple)') {
+            lastCircle.style.top = (dims.bottom - 50).toString() + 'px'
+
+        } else if ((event.clientY - 25 < (dims.top)) && lastCircle.style.background === 'var(--purple)') {
+            lastCircle.style.top = (dims.top).toString() + 'px'
+
         }
-    });
+
+    })
+
 }
 
 export function setBox() {
-    const centerBox = document.createElement('div');
-    centerBox.className = 'box';
-    document.body.append(centerBox);
+    const centerBox = document.createElement('div')
+    centerBox.setAttribute('class', 'box')
+    document.body.append(centerBox)
 }
