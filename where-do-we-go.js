f941cec383c404a80f5163e978147f9864ae6b28
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2023-12-10 16:50:56 +0000

    Update 'where-do-we-go.js'

--- where-do-we-go.js
+++ where-do-we-go.js
@@ -1,80 +1,123 @@
 import { places } from "./where-do-we-go.data.js";
 
-export function explore() {
-  const orderedPlaces = places.sort(compareCoordinates);
+var scroll = window.scrollY;
+const location = document.createElement("a");
+location.classList.add("location");
+document.body.appendChild(location);
 
-  const compass = createCompass();
-  let oldValue = 0;
-  let scrolled = false;
+document.addEventListener("DOMContentLoaded", () => {
+    selectPlace();
+});
 
-  window.addEventListener('scroll', () => {
-    let newValue = window.pageYOffset;
-    scrolled = true;
-    compass.innerHTML = oldValue < newValue ? 'S' : 'N';
-    oldValue = newValue;
-  });
+document.addEventListener("scroll", () => {
+    selectPlace();
+    scroll > window.scrollY
+        ? (document.querySelector(".direction").innerHTML = "N")
+        : (document.querySelector(".direction").innerHTML = "S");
+    scroll = window.scrollY;
+});
 
-  const locationAnchor = createLocationAnchor(orderedPlaces[0]);
-  document.body.append(locationAnchor);
-
-  createSections(orderedPlaces);
-
-  let observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting === true) {
-        let val = Math.round(window.scrollY / window.innerHeight);
-        updateLocationAnchor(locationAnchor, orderedPlaces[val]);
-      }
+function explore() {
+    // Places/sections
+    places.sort(compareCoordinates);
+    console.log(places);
+    places.forEach((place) => {
+        createSection(place);
     });
-  }, { threshold: [0.5] });
+    // Compass
+    const compass = document.createElement("div");
+    compass.classList.add("direction");
+    document.body.appendChild(compass);
+}
 
-  document.querySelectorAll('section').forEach(section => observer.observe(section));
+function createSection(place) {
+    let section = document.createElement("section");
+    section.style.background = `url('./where-do-we-go_images/${
+        place.name.toLowerCase().replaceAll(/ /g, "-").split(",")[0]
+    }.jpg')`;
+    section.style.backgroundSize = "cover";
+    section.style.backgroundPosition = "center";
+    section.style.backgroundRepeat = "no-repeat";
+    section.style.width = "100%";
+    section.style.height = "100vh";
+    document.body.appendChild(section);
+}
+
+function selectPlace() {
+    const sectionHeight = window.innerHeight;
+    const scroll = window.scrollY + sectionHeight / 2;
+    const sectionIndex = Math.floor(scroll / sectionHeight);
+    const place = places[sectionIndex];
+    location.textContent = `${place.name}\n${place.coordinates}`;
+    location.href = `https://www.google.com/maps/place/${urlEncodeCoordinates(
+        place.coordinates
+    )}/`;
+    console.log(
+        location.href
+            .split("%C2%B0")
+            .join("°")
+            .split("%22")
+            .join('"')
+            .split("%20")
+            .join(" ")
+    );
+    location.target = "_blank";
+    location.style.color = place.color;
+}
+
+function urlEncodeCoordinates(coordinates) {
+    return coordinates
+        .replaceAll(" ", "%20")
+        .replaceAll("°", "%C2%B0")
+        .replaceAll('"', "%22");
 }
 
 function compareCoordinates(a, b) {
-  const aCoords = getCoords(a.coordinates);
-  const bCoords = getCoords(b.coordinates);
-
-  return aCoords.localeCompare(bCoords);
+    const aDirection = a.coordinates.split(" ")[0].split('"')[1];
+    const bDirection = b.coordinates.split(" ")[0].split('"')[1];
+    const aLat = a.coordinates.split(" ")[0];
+    const bLat = b.coordinates.split(" ")[0];
+    let aLatDeg = parseInt(aLat.split("°")[0]);
+    let aLatMin = parseInt(aLat.split("°")[1].split("'")[0]);
+    let aLatSec = parseInt(aLat.split("°")[1].split("'")[1].split('"')[0]);
+    let bLatDeg = parseInt(bLat.split("°")[0]);
+    let bLatMin = parseInt(bLat.split("°")[1].split("'")[0]);
+    let bLatSec = parseInt(bLat.split("°")[1].split("'")[1].split('"')[0]);
+    if (aDirection === "S") {
+        aLatDeg = -aLatDeg;
+        aLatMin = -aLatMin;
+        aLatSec = -aLatSec;
+    }
+    if (bDirection === "S") {
+        bLatDeg = -bLatDeg;
+        bLatMin = -bLatMin;
+        bLatSec = -bLatSec;
+    }
+    if (aLatDeg > bLatDeg) {
+        return -1;
+    }
+    if (aLatDeg < bLatDeg) {
+        return 1;
+    }
+    if (aLatDeg === bLatDeg) {
+        if (aLatMin > bLatMin) {
+            return -1;
+        }
+        if (aLatMin < bLatMin) {
+            return 1;
+        }
+        if (aLatMin === bLatMin) {
+            if (aLatSec > bLatSec) {
+                return 1;
+            }
+            if (aLatSec < bLatSec) {
+                return -1;
+            }
+            if (aLatSec === bLatSec) {
+                return 0;
+            }
+        }
+    }
 }
 
-function getCoords(coordString) {
-  const [deg, min, sec, dir] = coordString.match(/\d+/g).map(Number);
-  const correctedDeg = dir === 'S' ? -deg : deg;
-  return `${correctedDeg}${min}${sec}`;
-}
-
-function createCompass() {
-  const compass = document.createElement('div');
-  compass.className = 'direction';
-  compass.innerHTML = 'N';
-  document.body.append(compass);
-  return compass;
-}
-
-function createLocationAnchor(place) {
-  const locationAnchor = document.createElement('a');
-  locationAnchor.className = 'location';
-  locationAnchor.innerHTML = `${place.name}\n${place.coordinates}`;
-  locationAnchor.style.color = place.color;
-  locationAnchor.href = `https://www.google.com/maps/place/${place.coordinates}`;
-  locationAnchor.target = '_blank';
-  return locationAnchor;
-}
-
-function updateLocationAnchor(anchor, place) {
-  anchor.innerHTML = `${place.name}\n${place.coordinates}`;
-  anchor.style.color = place.color;
-  anchor.href = `https://www.google.com/maps/place/${place.coordinates}`;
-  console.log('TEST', anchor.href.replaceAll('%C2%B0', '°').replaceAll('%22', '"').replaceAll('%20', ' '));
-}
-
-function createSections(places) {
-  places.forEach((place, index) => {
-    const newSection = document.createElement('section');
-    newSection.id = index.toString();
-    const imageLoc = place.name.toLowerCase().split(',')[0].split(' ').join('-');
-    newSection.style.background = `URL('./where-do-we-go_images/${imageLoc}.jpg')`;
-    document.body.append(newSection);
-  });
-}
+export { explore };
