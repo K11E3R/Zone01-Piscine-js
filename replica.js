37abea1bd1ce89c918ab943c402ee5035d7a1b8b
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2023-12-11 18:11:28 +0000

    Update 'replica.js'

--- replica.js
+++ replica.js
@@ -1,14 +1,22 @@
-const replica = (...objs) => objs.reduce((res, obj) => {
-    Object.keys(obj).forEach(key => {
-        if (Object.prototype.toString.call(obj[key]).slice(8, -1) === 'Object') {
-            res[key] = replica(res[key] || {}, obj[key]);
-        } else if (Array.isArray(obj[key])) {
-            res[key] = obj[key].map(item => Object.prototype.toString.call(item).slice(8, -1) === 'Object' ? replica({}, item) : item);
-        } else if (Object.prototype.toString.call(res[key]).slice(8, -1) === 'Object' && Object.prototype.toString.call(obj[key]).slice(8, -1) === 'Object') {
-            Object.keys(obj[key]).forEach(val => res[key][val] = obj[key][val]);
-        } else {
-            res[key] = obj[key];
+function replica(target, ...sources) {
+    sources.forEach(source => {
+        for (const key in source) {
+            if (Object.prototype.hasOwnProperty.call(source, key)) {
+                if (Object.prototype.toString.call(source[key]).slice(8, -1) === 'Object') {
+                    target[key] = target[key] || {};
+                    replica(target[key], source[key]);
+                } else if (Array.isArray(source[key])) {
+                    target[key] = source[key].map(item =>
+                        Object.prototype.toString.call(item).slice(8, -1) === 'Object'
+                            ? replica({}, item)
+                            : item
+                    );
+                } else {
+                    target[key] = source[key];
+                }
+            }
         }
     });
-    return res;
-}, {});
+
+    return target;
+}
