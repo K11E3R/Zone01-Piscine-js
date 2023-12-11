674002c75a1ef0aa295f5cfc042dc1638e557630
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2023-12-11 17:58:35 +0000

    Update 'pick-omit.js'

--- pick-omit.js
+++ pick-omit.js
@@ -1,7 +1,13 @@
 function pick(obj, keys) {
-    return Object.fromEntries(Object.entries(obj).filter(([key]) => (typeof keys === "string" ? keys.includes(key) : keys.includes(key))));
+    const elems_0 = {};
+    for (const key of Object.keys(obj).filter(k => (typeof keys === "string" ? keys.match(new RegExp(`^${k}$`)) : keys.includes(k))))
+        elems_0[key] = obj[key];
+    return elems_0;
 }
 
 function omit(obj, keys) {
-    return Object.fromEntries(Object.entries(obj).filter(([key]) => (typeof keys === "string" ? !keys.includes(key) : !keys.includes(key))));
+    const elems_0 = {};
+    for (const key of Object.keys(obj).filter(k => (typeof keys === "string" ? !keys.match(new RegExp(`^${k}$`)) : !keys.includes(k))))
+        elems_0[key] = obj[key];
+    return elems_0;
 }
