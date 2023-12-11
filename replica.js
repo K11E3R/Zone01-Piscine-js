b7eaee65e278c1ae5bdd81593113f14b87e21269
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2023-12-11 18:15:10 +0000

    Update 'replica.js'

--- replica.js
+++ replica.js
@@ -2,6 +2,14 @@ function replica(elem_to, ...elem_from) {
     elem_from.forEach(source => {
         for (const key in source) {
             if (Object.prototype.hasOwnProperty.call(source, key)) {
+                if (
+                    Object.prototype.hasOwnProperty.call(elem_to, key) &&
+                    Object.prototype.toString.call(elem_to[key]).slice(8, -1) !== 'Object'
+                ) {
+                    // If elem_to[key] is not an object, create a new object
+                    elem_to[key] = {};
+                }
+
                 if (Object.prototype.toString.call(source[key]).slice(8, -1) === 'Object') {
                     elem_to[key] = elem_to[key] || {};
                     replica(elem_to[key], source[key]);
