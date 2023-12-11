5b6802f3006c7caf70f68a4f422c1322f5c9c26a
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2023-12-11 18:28:52 +0000

    Update 'neuron.js'

--- neuron.js
+++ neuron.js
@@ -4,17 +4,17 @@ function neuron(arr) {
     for (const item of arr) {
         const [type, key, response] = parseItem(item);
 
-        res[type][key] ||= { [type.slice(0, -1)]: key.replaceAll(' ', '_'), responses: [] };
+        res[type][key] ||= { [type.slice(0, -1)]: key, responses: [] };
         res[type][key].responses.push(response);
     }
 
     return res;
 }
 
-function parseItem(arr) {
-    const [type, ...rest] = arr.split(' ');
-    const key = rest.join(' ').split('-')[0].slice(0, -1);
-    const response = arr.includes('-') ? arr.split('-')[1].slice(1).split(' ').slice(1).join(' ') : '';
+function parseItem(item) {
+    const [type, ...rest] = item.split(' ');
+    const key = rest.join(' ').split('-')[0].slice(0, -1).replaceAll(' ', '_').toLowerCase();
+    const response = item.includes('-') ? item.split('-')[1].slice(1).trim() : '';
 
-    return [type.toLowerCase() + 's', key.toLowerCase(), response];
+    return [type.toLowerCase() + 's', key, response];
 }
