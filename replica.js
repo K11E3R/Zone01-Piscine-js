6a4cef5d54c59b6d57cb4a19d4d4b598e8d716ec
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2023-12-11 18:12:58 +0000

    Update 'replica.js'

--- replica.js
+++ replica.js
@@ -1,22 +1,22 @@
-function replica(tt, ...sources) {
-    sources.forEach(source => {
+function replica(elem_to, ...elem_from) {
+    elem_from.forEach(source => {
         for (const key in source) {
             if (Object.prototype.hasOwnProperty.call(source, key)) {
                 if (Object.prototype.toString.call(source[key]).slice(8, -1) === 'Object') {
-                    tt[key] = tt[key] || {};
-                    replica(tt[key], source[key]);
+                    elem_to[key] = elem_to[key] || {};
+                    replica(elem_to[key], source[key]);
                 } else if (Array.isArray(source[key])) {
-                    tt[key] = source[key].map(item =>
+                    elem_to[key] = source[key].map(item =>
                         Object.prototype.toString.call(item).slice(8, -1) === 'Object'
                             ? replica({}, item)
                             : item
                     );
                 } else {
-                    tt[key] = source[key];
+                    elem_to[key] = source[key];
                 }
             }
         }
     });
 
-    return tt;
+    return elem_to;
 }
