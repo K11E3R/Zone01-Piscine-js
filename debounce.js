03c7bd2dbac13ffb5ed1b15556a733d5cef0f3e8
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2023-12-15 19:24:37 +0000

    Update 'debounce.js'

--- debounce.js
+++ debounce.js
@@ -1,17 +1,17 @@
-function debounce(f, t) 
-  let t_out;
+function debounce(f, t) {
+  let time_out;
   return function() {
-    clearTimeout(t_out);
-    t_out = setTimeout(() => f.apply(this, arguments), t || 100);
+    clearTimeout(time_out);
+    time_out = setTimeout(() => f.apply(this, arguments), t || 100);
   };
 }
 
 function opDebounce(f, w, i) {
-  let t_out;
+  let time_out;
   return function() {
-    const c = this, args = arguments, n = i && !t_out;
-    clearTimeout(t_out);
-    t_out = setTimeout(() => {t_out = null; if (!i) f.apply(c, args);}, w);
+    const c = this, args = arguments, n = i && !time_out;
+    clearTimeout(time_out);
+    time_out = setTimeout(() => {time_out = null; if (!i) f.apply(c, args);}, w);
     if (n) f.apply(c, args);
   };
 }
