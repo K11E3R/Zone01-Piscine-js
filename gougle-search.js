2e86c6af9530ed48d629b353dad2d69c1f3262d0
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2023-12-15 19:46:57 +0000

    Update 'gougle-search.js'

--- gougle-search.js
+++ gougle-search.js
@@ -1,16 +1,16 @@
-const queryServers = async (serverName, q) =>
-  await Promise.race([getJSON(`/${serverName}?q=${q}`), getJSON(`/${serverName}_backup?q=${q}`)]);
+const queryServers = async (srv_name, q) =>
+  await Promise.race([getJSON(`/${srv_name}?q=${q}`), getJSON(`/${srv_name}_backup?q=${q}`)]);
 
 const gougleSearch = async (q) => {
-  const servers = ['web', 'image', 'video'];
+  const srvs = ['web', 'image', 'video'];
   const timeoutPromise = new Promise((_, reject) => setTimeout(reject, 80, new Error('timeout')));
-  const fastestServers = servers
-    .map((serverName) => queryServers(serverName, q)) // (my_collector)
+  const fastestServers = srvs
+    .map((srv_name) => queryServers(srv_name, q)) // (my_collector)
     .map(async (promise) => await Promise.race([promise, timeoutPromise]));
 
-  const result = {};
-  for (let i = 0; i < servers.length; i++) {
-    result[servers[i]] = await fastestServers[i];
+  const res = {};
+  for (let i = 0; i < srvs.length; i++) {
+    res[srvs[i]] = await fastestServers[i];
   }
-  return result;
+  return res;
 };
