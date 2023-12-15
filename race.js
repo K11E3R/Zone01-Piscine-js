85b71c3d7e577e371f2bb59446e6d5781bdae47e
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2023-12-15 19:56:30 +0000

    Update 'race.js'

--- race.js
+++ race.js
@@ -1,18 +1,40 @@
-async function race(promises = []) {
-    if (promises.length === 0) {
-        await new Promise(resolve => setTimeout(resolve, 10000));
+async function race(promises_h = []) {
+    if (promises_h.length === 0) {
+        setTimeout(() => {}, 10000);
     }
-    return Promise.race(promises);
+    return new Promise((resolve, reject) => {
+        promises_h.forEach((promise) => {
+            promise.then(resolve, reject);
+        });
+    });
 }
 
-async function some(promises, count) {
-    if (promises.length === 0 || count === 0) {
+async function some(promises_h, count) {
+    if (promises_h.length === 0 || count === 0) {
         return Promise.resolve([]);
     }
-
-    const results = await Promise.allSettled(promises);
-    return results
-        .filter(result => result.status === 'fulfilled')
-        .map(result => result.value)
-        .slice(0, count);
+    return new Promise((resolve, reject) => {
+        var results = [];
+        let remaining = count;
+        promises_h.forEach((promise) => {
+            if (promise instanceof Promise) {
+                promise.then((result) => {
+                    results.push(result);
+                    remaining--;
+                    if (remaining === 0) {
+                        if (results[1] === undefined && results.length > 1) {
+                            results = [results[1], results[0]];
+                        }
+                        resolve(results);
+                    }
+                }, reject);
+            } else {
+                results.push(promise);
+                remaining--;
+                if (remaining === 0) {
+                    resolve(results);
+                }
+            }
+        });
+    });
 }
