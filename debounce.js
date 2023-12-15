aa6a9a975802f2731b963559a77137be7127a717
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2023-12-15 19:19:45 +0000

    Update 'debounce.js'

--- debounce.js
+++ debounce.js
@@ -5,3 +5,14 @@ const debounce = (func, timer_t) => {
         timer = setTimeout(() => func(...args), timer_t)
     }
 }
+
+
+const opDebounce = (func, time, options) => {
+    let timer
+    options === undefined ? options = { leading: false } : null
+    return function (...args) {
+        if (!timer && options.leading) func(...args)
+        clearTimeout(timer)
+        timer = setTimeout(() => func(...args), time)
+    }
+}
